28129b1bd0652614204083945c45ecefc320b218
mayor167 <adenijimayokun@gmail.com>
2023-06-16 03:14:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ *  main - A program that print a line to the standard output
+ *  Return: 1 (Success)
+ */
+int main(void)
+{
+printline("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
