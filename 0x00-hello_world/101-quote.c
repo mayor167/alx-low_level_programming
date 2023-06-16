2c48bd3893ff013bcfc84ef712beb6162a640a3b
mayor167 <adenijimayokun@gmail.com>
2023-06-16 03:21:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-printline("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+printl("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
