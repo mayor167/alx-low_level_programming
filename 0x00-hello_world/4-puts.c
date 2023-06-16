ecac24ba971d81c31489b92b63c835cfa05cb891
mayor167 <adenijimayokun@gmail.com>
2023-06-15 21:35:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main -prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
  * Return: 0 (success)
  */
-init main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);	   
