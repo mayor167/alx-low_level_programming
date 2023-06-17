8622df8f17be3d01dceef1076179e514176abd52
mayor167 <adenijimayokun@gmail.com>
2023-06-17 11:17:08 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main: A program that prints alphabets in lowercase and excludes some
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter == 'q' && letter == 'e')
+{
+continue;
+}
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
