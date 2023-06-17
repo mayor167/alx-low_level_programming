38dc1f94ab33c9d9a5a38096ae45e63ea52f5f3a
mayor167 <adenijimayokun@gmail.com>
2023-06-17 08:36:28 -0700

    #include <stdio.h>

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main:A program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = "a", letter <= "z", letter++)
+{
+putchar(letter);
+}
+}
