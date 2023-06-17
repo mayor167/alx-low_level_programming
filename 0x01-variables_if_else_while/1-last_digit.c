a8ded49e461d60f74cb1b3078748f5bca1169fb1
mayor167 <adenijimayokun@gmail.com>
2023-06-17 06:56:18 -0700

    The last digit of n is calculated using the modulo operator %

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,17 @@
 int main(void)
 {
 	int n;
-
+int l;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-int l;
 l = n % 10;
 if (l > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 
-else if (l < 6 && != 0)
+else if (l < 6 && l != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
