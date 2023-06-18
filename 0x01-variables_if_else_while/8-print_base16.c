1f41f097702022a6039c01097326f95d6df0e9aa
mayor167 <adenijimayokun@gmail.com>
2023-06-18 13:24:08 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * Main:A program that prints all number in base 16
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int num_in_b16;
+for (num_in_b16 = 0; num_in_b16 < 16; num_in_b16++)
+{
+if (num_in_b16 <= 9)
+{
+putchar(num_in_b16);
+}
+else
+{
+putchar(num_in_b16);
+}
+}
+putchar('\n')
+return (0);
+}
