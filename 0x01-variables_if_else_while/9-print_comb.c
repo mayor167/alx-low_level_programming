ca000fef80488df070a419bddb61caf880c521aa
mayor167 <adenijimayokun@gmail.com>
2023-06-18 14:59:44 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@ int main(void)
 int single_digit;
 for (single_digit = 0; single_digit < 9; single_digit++)
 {
-putchar(single_digit + '0' + ',');
+putchar(single_digit + ',' + '0');
 }
 putchar('\n');
 return (0);
