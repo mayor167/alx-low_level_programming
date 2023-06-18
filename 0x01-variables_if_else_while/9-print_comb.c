22ce3d2df8473c10a7e0410f992a93bf8931d581
mayor167 <adenijimayokun@gmail.com>
2023-06-18 15:08:51 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int single_digit;
-for (single_digit = 0; single_digit < 9; single_digit++)
+for (single_digit = 0; single_digit < 10; single_digit++)
 {
 putchar(single_digit + '0');
 putchar(',');
