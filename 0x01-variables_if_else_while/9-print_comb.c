b5f918ce942e73c7b111aaf0d895dbea905fd576
mayor167 <adenijimayokun@gmail.com>
2023-06-18 14:57:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints combination of single digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int single_digit;
+for (single_digit = 0; single_digit < 9; single_digit++)
+{
+putchar(single_digit + '0' + ',');
+}
+putchar('\n');
+return (0);
+}
