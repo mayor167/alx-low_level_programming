62987d42f7e03c777ce08e327a03ba72200040c2
mayor167 <adenijimayokun@gmail.com>
2023-06-19 08:42:26 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,11 +21,11 @@ int k;
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-						if (i != 7 || j != 8 || k != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 				}
 			}
 		}
