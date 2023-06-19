14b9399be6a8eae50c46b892769c472b76a45be5
mayor167 <adenijimayokun@gmail.com>
2023-06-19 01:56:46 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -A program that prints the smallest combibation of two digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+
+	{
+	for (j = 0; j < 10; j++)
+	{
+		if (i == j)
+		continue;
+		else
+	putchar(ij + '0');
+	}
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
