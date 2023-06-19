7e06870e43b2faf69b69c11bb0351a0d23ed7b66
mayor167 <adenijimayokun@gmail.com>
2023-06-19 04:11:48 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,12 @@ int main(void)
 		for (j = i + 1; j <= 9; j++)
 		{
 			if (i == j)
+				{
 				continue;
 
 				putchar(i + '0');
 				putchar(j + '0');
 				if (i != 8 && j != 9)
-				{
 					putchar(',');
 					putchar(' ');
 				}
