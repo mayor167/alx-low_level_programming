6ce715d974b38109694334bd467633be034b403c
mayor167 <adenijimayokun@gmail.com>
2023-06-19 14:33:39 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,15 +8,19 @@ int main(void)
 {
 int i;
 int j;
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i <= 99; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-		putchar(i + '0');
-		putchar(j + '0');
-			if (!(i == 98 && j == 99))
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+			if (i != 98 && j != 99)
 			{
 				putchar(',');
+				putcharr(' ');
 			}
 		}
 	}
