3ae890a9052ef9f39f5658b93bccdd168b1f89c0
mayor167 <adenijimayokun@gmail.com>
2023-06-21 07:13:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,15 @@
  */
 int main(void)
 {
-char message[] = "_putchar";
-printf("%s\n", message);
+_putchar('_');
+_putchar('p';)
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 
 }
