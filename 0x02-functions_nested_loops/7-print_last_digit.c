352fa3ab1ac876db0c8cf7bbe5a26b790d20e6da
mayor167 <adenijimayokun@gmail.com>
2023-06-21 16:07:48 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,10 @@ int print_last_digit(int num)
 {
 	int last_digit = num % 10;
 
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
