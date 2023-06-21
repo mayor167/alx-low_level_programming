6a9939060afc649049cdf5f30559107b9b8e63ce
mayor167 <adenijimayokun@gmail.com>
2023-06-21 15:13:01 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs -A program that computes absolute value of an integer
+ *@num: The character to be checked
+ *Return: - num, otherwise num
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return (-num);
+	else
+		return (num);
+}
