cef6721b09506e6f50c835fc8be999a714fc4447
mayor167 <adenijimayokun@gmail.com>
2023-06-21 01:33:05 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -A program that prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char message[] = "_putchar";
+printf("%s\n", message);
+return (0);
+
+}
