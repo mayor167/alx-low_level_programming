d3794b7d9896fbf58c0e9163ebf05c13d0f0f031
mayor167 <adenijimayokun@gmail.com>
2023-06-22 08:55:45 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,5 +6,14 @@
  */
 void print_most_numbers(void)
 {
+	int count;
 
+	for (count = 0; count <= 9; count++)
+	{
+		if (count == 2 || count == 4)
+			continue;
+		else
+			_putchar(count + '0');
+	}
+	_putchar('\n');
 }
