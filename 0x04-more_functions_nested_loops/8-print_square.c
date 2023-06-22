5b4fd4abff46258519f5e11264b5392a112a2155
mayor167 <adenijimayokun@gmail.com>
2023-06-22 16:32:49 -0700

    8-squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,5 +7,19 @@
  */
 void print_square(int size)
 {
+	int row;
+	int column;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
