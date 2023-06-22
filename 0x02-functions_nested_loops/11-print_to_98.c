390de4dd893332e391e67c797348a1694c11ffa1
mayor167 <adenijimayokun@gmail.com>
2023-06-21 19:43:47 -0700

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to be considered
@@ -28,7 +29,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				prinf("%d", n);
+				printf("%d", n);
 				printf("\n");
 				break;
 			}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef main_h
 #define main_h
 
