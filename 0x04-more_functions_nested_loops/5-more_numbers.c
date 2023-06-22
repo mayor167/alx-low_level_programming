4dec4d2f02f141f2ca618b025914a65644a8f43d
mayor167 <adenijimayokun@gmail.com>
2023-06-22 11:13:48 -0700

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,5 +6,21 @@
  */
 void more_numbers(void)
 {
+	int num_of_time;
+	int number;
 
+	for (num_of_time = 0; num_of_time < 10; num_of_time++)
+	{
+		for (number = 0; number < 15; number++)
+		{
+			if (number > 9)
+			{
+			_putchar('0' + (number / 10));
+			}
+			else
+			_putchar('0' + (number % 10));
+		}
+
+	_putchar('\n');
+	}
 }
