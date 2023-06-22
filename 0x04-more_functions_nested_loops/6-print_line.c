46d4bfa21ace5a9f77cb9ae1fcf66ccadad052d3
mayor167 <adenijimayokun@gmail.com>
2023-06-22 15:48:25 -0700

    u

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,13 @@
  */
 void print_line(int n)
 {
-	while (n > 0)
-	{	_putchar('_');
-		n--;
-	}
 	if (n <= 0)
 	{
 	_putchar('\n');
+	}
+	while (n > 0)
+	{	_putchar('_');
+		n--;
 	}
 	_putchar('\n');
 }
