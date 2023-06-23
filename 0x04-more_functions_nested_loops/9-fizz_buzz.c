1d879e1e634369f961585f1bd5045bc3e0895a8b
mayor167 <adenijimayokun@gmail.com>
2023-06-22 17:42:16 -0700

    m

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,32 @@
  *
  * Return: 0 (Success)
  */
-void main(void)
+int  main(void)
 {
+	int count;
+	char mult_of_3[] = "Fizz";
+	char mult_of_5[] = "Buzz";
+	char mult_of_5_and_3 = "FizzBuzz";
 
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0)
+		{
+			printf("%s", mult_of_3);
+		}
+		else if (count % 5 == 0)
+		{
+			printf("%s", mult_of_5);
+		}
+		else if (count % 5 == 0 && count % 3 == 0)
+		{
+			printf("%s", mult_of_5_and_3);
+		}
+		else
+		{
+			printf("%d", count);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
 }
