af12095abc976dbf7dbb6f6bb1c9af8dbeeea88f
mayor167 <adenijimayokun@gmail.com>
2023-06-22 18:43:58 -0700

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,19 @@
  */
 void print_triangle(int size)
 {
+	int row;
+	int column;
 
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for (row = 0; row <= size; row++)
+	{
+		for (column = 0 ; column <= row; column++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
