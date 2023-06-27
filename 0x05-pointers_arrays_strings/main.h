5c2411802541e2578d167103ddcba1c32ef1ff57
mayor167 <adenijimayokun@gmail.com>
2023-06-27 05:19:38 -0700

    d

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
-# ifdef main_h
-# define
-# include <stdio.h>
+# ifndef MAIN_H
+# define MAIN_H
+
+
+#include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
