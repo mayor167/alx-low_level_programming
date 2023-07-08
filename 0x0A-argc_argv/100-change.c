566d3279f149c62878a4b8a0f7d7ca8f5537976a
mayor167 <adenijimayokun@gmail.com>
2023-07-07 19:10:28 -0700

    half done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+/**
+ * main - Takes in exactly one argument for minimum coin count
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+}
