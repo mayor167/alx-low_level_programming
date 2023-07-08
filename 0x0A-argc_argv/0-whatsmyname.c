c9901eca71f21fb0717c149dd501251a988b3067
mayor167 <adenijimayokun@gmail.com>
2023-07-07 17:28:35 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - A program that prints program name
+ *@argc: number of command line
+ *@argv: array of name
+ *Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
