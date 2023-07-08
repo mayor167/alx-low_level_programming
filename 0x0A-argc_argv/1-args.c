3ac999bfe719c0e6275c5114f9553612fac83d5c
mayor167 <adenijimayokun@gmail.com>
2023-07-07 18:45:12 -0700

    h

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * main - Prints the number of arguments passed into main
+ * main - program that Prints the number of arguments passed into main
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: 0
+ * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
@@ -13,12 +13,14 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc == 1)
+		/**
+		 * 1 from substracted from argc to deduct the program name
+		 */
 		printf("%d\n", argc - 1);
 	else
 	{
 		for (i = 0; *argv; i++, argv++)
 			;
-
 		printf("%d\n", i - 1);
 	}
 
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+
+	return (0);
+}
