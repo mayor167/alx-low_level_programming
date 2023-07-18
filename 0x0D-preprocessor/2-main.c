a41314ec28a455bf5b6f18b03bf6a1adc2bc083a
mayor167 <adenijimayokun@gmail.com>
2023-07-18 14:05:38 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - A program that prints the name of the file it was compiled from
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
